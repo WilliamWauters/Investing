bfe89a7c5ff12ec972836b5972695ceafcf6c553
WilliamGordon <william.wauters@outlook.com>
2023-02-26 14:48:12 +0100

    Add Chart Js

--- src/components/forms/housing/HousingResultsChart.tsx
+++ src/components/forms/housing/HousingResultsChart.tsx
@@ -2,34 +2,92 @@ import { useHousingForm } from "@/contexts/HousingFormContext";
 import { getNotaryFee, getRegistrationFee, getTVA } from "@/utils/calculation";
 import formatMoney from "@/utils/formatMoney";
 import { Box } from "@mui/material";
-import { Cell, Label, Legend, Pie, PieChart } from "recharts";
+import "chart.js/auto";
+import { Doughnut } from "react-chartjs-2";
 
-const data = [
-  { name: "Group A", value: 400 },
-  { name: "Group B", value: 300 },
-  { name: "Group C", value: 300 },
-  { name: "Group D", value: 200 },
+export const datatest = {
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      // backgroundColor: [
+      //   "rgba(255, 99, 132, 0.2)",
+      //   "rgba(54, 162, 235, 0.2)",
+      //   "rgba(255, 206, 86, 0.2)",
+      //   "rgba(75, 192, 192, 0.2)",
+      //   "rgba(153, 102, 255, 0.2)",
+      //   "rgba(255, 159, 64, 0.2)",
+      // ],
+      // borderColor: [
+      //   "rgba(255, 99, 132, 1)",
+      //   "rgba(54, 162, 235, 1)",
+      //   "rgba(255, 206, 86, 1)",
+      //   "rgba(75, 192, 192, 1)",
+      //   "rgba(153, 102, 255, 1)",
+      //   "rgba(255, 159, 64, 1)",
+      // ],
+      borderWidth: 1,
+    },
+  ],
+};
+const plugins = [
+  {
+    beforeDraw: function (chart) {
+      var width = chart.width,
+        height = chart.height,
+        ctx = chart.ctx;
+      ctx.restore();
+      var fontSize = (height / 160).toFixed(2);
+      ctx.font = fontSize + "em sans-serif";
+      ctx.textBaseline = "top";
+      var text = "Foo-bar",
+        textX = Math.round((width - ctx.measureText(text).width) / 2),
+        textY = height / 2;
+      ctx.fillText(text, textX, textY);
+      ctx.save();
+    },
+  },
 ];
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
 const HousingResultsChart = () => {
   const { HousingFormState } = useHousingForm();
 
-  console.log(HousingFormState);
+  const registrationFee = getRegistrationFee(
+    HousingFormState.price,
+    HousingFormState.isEntiteldToReduction
+  );
+  const notaryFee = getNotaryFee(HousingFormState.price);
+  const TVA = getTVA(HousingFormState.price);
 
-  const data = [
-    {
-      name: "registrationFee ",
-      value: getRegistrationFee(
-        HousingFormState.price,
-        HousingFormState.isEntiteldToReduction
-      ),
-    },
-    { name: "notaryFee", value: getNotaryFee(HousingFormState.price) },
-    { name: "TVA", value: getTVA(HousingFormState.price) },
-  ];
+  console.log("registrationFee", formatMoney(registrationFee));
 
-  const total = data.reduce((a, b) => a + b.value, 0);
+  const data = {
+    labels: [
+      "Registration Fee - " + formatMoney(registrationFee),
+      "Notary Fee - " + formatMoney(notaryFee),
+      "TVA - " + formatMoney(TVA),
+    ],
+    datasets: [
+      {
+        label: "Fees",
+        data: [registrationFee, notaryFee, TVA],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const total = data.datasets[0].data.reduce((a, b) => a + b, 0);
 
   return (
     <>
@@ -40,35 +98,25 @@ const HousingResultsChart = () => {
           display: "flex",
           direction: "column",
           justifyContent: "center",
+          height: "450px",
         }}
       >
-        <PieChart
-          width={250}
-          height={400}
-          // style={{ border: "1px solid white" }}
-        >
-          <Legend verticalAlign="top" height={36} />
-          <Pie
-            data={data}
-            dataKey="value"
-            nameKey="name"
-            cx={120}
-            cy={200}
-            innerRadius={60}
-            outerRadius={80}
-            fill="#8884d8"
-            label
-            paddingAngle={5}
-          >
-            <Label value={total} position="center" />
-            {data.map((entry, index) => (
-              <Cell
-                key={`cell-${index}`}
-                fill={COLORS[index % COLORS.length]}
-              />
-            ))}
-          </Pie>
-        </PieChart>
+        <Doughnut
+          data={data}
+          options={{
+            responsive: true,
+            plugins: {
+              title: {
+                display: true,
+                text: "TOTAL " + formatMoney(total),
+              },
+              legend: {
+                position: "left",
+                align: "center",
+              },
+            },
+          }}
+        />
       </Box>
     </>
   );
--- src/utils/formatMoney.tsx
+++ src/utils/formatMoney.tsx
@@ -1,19 +1,6 @@
-import { Typography } from "@mui/material";
-import { NumericFormat } from "react-number-format";
-
-function formatMoney(value: number) {
-  return (
-    <Typography component="span" sx={{ fontWeight: "bold", color: "#38BDF8" }}>
-      <NumericFormat
-        value={value}
-        displayType={"text"}
-        thousandSeparator={"."}
-        decimalSeparator={","}
-        prefix={" "}
-        suffix={" €"}
-      />
-    </Typography>
+const formatMoney = (value: number) =>
+  new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(
+    value
   );
-}
 
 export default formatMoney;

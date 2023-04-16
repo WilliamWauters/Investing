ed056356ac7dc11726843e6e953616a965a24205
WilliamGordon <william.wauters@outlook.com>
2023-04-16 08:29:32 +0200

    Fix CSS rendering issue

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,11 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html
-      lang="en"
-      className="dark"
-      style={{ height: "100%", overflow: "auto" }}
-    >
+    <Html lang="en" className="dark">
       <Head />
       <body className="dark:bg-slate-900 text-white font-mono">
         <Main />
